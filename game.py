06fc157db2ca4f2d50ca721ac6481802bae488f3
tioBlachi <blasantunez310@gmail.com>
2024-09-03 17:30:24 -0400

    Update game.py
    Formatting reset() upon user selecting 'n'

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import random
 
-suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-cards = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+suits = ['♣️', '♦️', '❤️', '♠️']
+cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 
 class Blackjack:
     def __init__(self, playerHandVal=0, dealerHandVal=0):
@@ -12,6 +12,7 @@ class Blackjack:
         self.playerWins = 0
         self.dealerWins = 0
         self.ties = 0
+        self.bank = 100
         self.totalGames = 1
         self.playerTurn = True
         self.dealerTurn = False
@@ -19,15 +20,15 @@ class Blackjack:
         self.dealerBlackjack = False
 
     def card_value(self, card):
-        if card[0] in ["Jack", "Queen", "King"]:
+        if card[0] in ["J", "Q", "K"]:
             return 10
-        elif card[0] == "Ace" and self.playerHandVal <= 10:
+        elif card[0] == "A" and self.playerHandVal <= 10:
             return 11
-        elif card[0] == "Ace" and self.playerHandVal > 11:
+        elif card[0] == "A" and self.playerHandVal > 11:
             return 1
-        elif card[0] == "Ace" and self.dealerHandVal <= 10:
+        elif card[0] == "A" and self.dealerHandVal <= 10:
             return 11
-        elif card[0] == "Ace" and self.dealerHandVal > 11:
+        elif card[0] == "A" and self.dealerHandVal > 11:
             return 1
         else:
             return int(card[0])
@@ -44,7 +45,7 @@ class Blackjack:
         self.playerHandVal += value1 + value2
 
         print("\nYou have been dealt:")
-        print(f"{first[0]} of {first[1]} and {second[0]} of {second[1]}")
+        print(f"{first[0]}{first[1]} and {second[0]}{second[1]}")
         print(f"Hand Total: {self.playerHandVal}\n")
         self.check_blackjack()
 
@@ -61,20 +62,22 @@ class Blackjack:
     def hitPlayer(self):
         drawn_card = self.deck.pop()
         self.playerHandVal += self.card_value(drawn_card)
-        print(f"You drew a {drawn_card[0]} of {drawn_card[1]}")
-        print(f"Hand total: {self.playerHandVal}")
+        print(f"You drew a {drawn_card[0]}{drawn_card[1]}")
+        print(f"Hand total: {self.playerHandVal}\n")
 
     def hitDealer(self):
         while self.dealerHandVal < 17:
             drawn_card = self.deck.pop()
             self.dealerHandVal += self.card_value(drawn_card)
-            print(f"Dealer Drew a {drawn_card[0]} of {drawn_card[1]}")
-            print(f"Dealer Hand Total: {self.dealerHandVal}")
+            print(f"Dealer Drew a {drawn_card[0]}{drawn_card[1]}")
+            print(f"Dealer Hand Total: {self.dealerHandVal}\n")
+        if 21 > self.dealerHandVal >= 17:
+            print("Dealer must stand!")
 
     def check_blackjack(self):
         if self.playerHandVal == 21:
             self.playerWins += 1
-            print("Blackjack! You win!")
+            print("Blackjack! You win!\n")
             self.reset()
             return True
 
@@ -83,87 +86,103 @@ class Blackjack:
         if self.playerTurn:
             if self.playerHandVal > 21:
                 self.dealerWins += 1
-                print("BUST! You lose!")
+                print("BUST! You lose!\n")
                 self.reset()
 
         if not self.playerTurn:
             if self.playerHandVal > self.dealerHandVal:
-                print("You beat the dealer! You win!")
+                print("You beat the dealer! You win!\n")
                 self.playerWins += 1
                 self.reset()
 
             elif self.dealerHandVal == self.playerHandVal:
-                print("PUSH! It's a tie!")
+                print("PUSH! It's a tie!\n")
                 self.ties += 1
                 self.reset()
 
             elif self.dealerHandVal > 21 >= self.playerHandVal:
                 self.playerWins += 1
-                print("Dealer BUSTS! You win!")
+                print("Dealer BUSTS! You win!\n")
                 self.reset()
 
             elif self.playerHandVal < self.dealerHandVal <= 21:
                 self.dealerWins += 1
-                print("Dealer wins! You lose!")
+                print("Dealer wins! You lose!\n")
                 self.reset()
 
             elif self.playerHandVal > 21:
                 self.dealerWins += 1
-                print("BUST! You lose!")
+                print("BUST! You lose!\n")
                 self.reset()
 
     def showStats(self):
-        print("_______ STATS _______")
-        if self.dealerWins and self.playerWins == 0:
-            print("0.00% Player Wins")
-            print(f"Ties: {self.ties}")
-            print(f"Total Games: {self.totalGames - 1}")
-            print(f"Your current hand total: {self.playerHandVal}\n")
-        elif self.dealerWins == 0 and self.playerWins > 0:
-            print(f"{self.playerWins / self.totalGames * 100:.2f}% Player Wins")
-            print(f"Ties: {self.ties}")
-            print(f"Total Games: {self.totalGames - 1}")
-            print(f"Your current hand total: {self.playerHandVal}\n")
-        else:
-            print(f"Player Wins: {self.playerWins}")
-            print(f"Losses: {self.dealerWins}")
-            print(f"Ties: {self.ties}")
-            print(f"Total Games: {self.totalGames - 1}")
-            print(f"{self.playerWins / (self.totalGames - 1) * 100:.2f}% Player Wins")
-            print(f"Your current hand total: {self.playerHandVal}\n")
+        if self.running:
+            print("           STATS           ")
+            print("___________________________")
 
+            if self.dealerWins == 0 and self.playerWins == 0:
+                print("0.00% Player Wins")
+                print(f"Ties: {self.ties}")
+                print(f"Total Games: {self.totalGames - 1}")
+                print(f"Your current hand total: {self.playerHandVal}\n")
+            elif self.dealerWins == 0 and self.playerWins > 0 and self.totalGames == self.playerWins:
+                print(f"{self.playerWins / self.totalGames * 100:.2f}% Player Wins")
+                print(f"Losses: {self.dealerWins}")
+                print(f"Ties: {self.ties}")
+                print(f"Total Games: {self.totalGames - 1}")
+                print(f"Your current hand total: {self.playerHandVal}\n")
+            else:
+                print(f"Player Wins: {self.playerWins}")
+                print(f"Losses: {self.dealerWins}")
+                print(f"Ties: {self.ties}")
+                print(f"Total Games: {self.totalGames - 1}")
+                print(f"{self.playerWins / (self.totalGames - 1) * 100:.2f}% Player Wins")
+                print(f"Your current hand total: {self.playerHandVal}\n")
+        else:
+            print("           STATS           ")
+            print("___________________________")
+            if self.dealerWins and self.playerWins == 0:
+                print("0.00% Player Wins")
+                print(f"Ties: {self.ties}")
+                print(f"Total Games: {self.totalGames - 1}")
+            else:
+                #self.dealerWins == 0 and self.playerWins > 0 and self.totalGames == self.playerWins:
+                print(f"{self.playerWins / self.totalGames * 100:.2f}% Player Wins")
+                print(f"Ties: {self.ties}")
+                print(f"Total Games: {self.totalGames - 1}")
 
     def reset(self):
-    while True:
-        choice = input("Play again? (Y/N)").lower()
-        if choice == 'y':
-            self.playerHandVal = 0
-            self.dealerHandVal = 0
-            self.deck = [[card, suit] for card in cards for suit in suits]
-            self.deck = self.initGame()
-            self.playerTurn = True
-            self.dealerTurn = False
-            self.dealerHand = None
-            self.dealerBlackjack = False
-            self.totalGames += 1
-            print("********* BLACKJACK ***********")
-            print(f"           GAME #{self.totalGames}             ")
-            self.drawPlayerHand()
-            self.drawDealerHand()
-            break
-        elif choice == 'n':
-            self.running = False
-            self.playerTurn = False
-            self.dealerTurn = False
-            print("Thank you for playing!")
-            break
-        else:
-            print("Invalid input! Please enter either 'y' or 'n'.")
+        while True:
+            choice = input("Play again? (Y/N)").lower()
+            if choice == 'y':
+                self.playerHandVal = 0
+                self.dealerHandVal = 0
+                self.deck = [[card, suit] for card in cards for suit in suits]
+                self.deck = self.initGame()
+                self.playerTurn = True
+                self.dealerTurn = False
+                self.dealerHand = None
+                self.dealerBlackjack = False
+                self.totalGames += 1
+                print("********* BLACKJACK ***********")
+                print(f"           GAME #{self.totalGames}             ")
+                self.drawPlayerHand()
+                self.drawDealerHand()
+                break
+            elif choice == 'n':
+                self.running = False
+                self.playerTurn = False
+                self.dealerTurn = False
+                print("Thank you for playing!")
+                self.showStats()
+                break
+            else:
+                print("Invalid input! Please enter either 'y' or 'n'.\n")
 
     def check_dealer_blackjack(self):
         if self.dealerBlackjack:
             self.dealerWins += 1
-            print("Dealer has Blackjack! You lose!")
+            print("Dealer has Blackjack! You lose!\n")
             self.reset()
 
     def getPlayerTurn(self):
@@ -192,4 +211,5 @@ class Blackjack:
 
     def setDealerTurn(self, dealerTurn):
         self.dealerTurn = dealerTurn
+
 

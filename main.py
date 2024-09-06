b7750026bba6d708d4621acb12f94afa40e50c59
tioBlachi <blasantunez310@gmail.com>
2024-09-06 15:00:19 -0400

    Update main.py
    main.py only initializes the game instance of the imported Game class and runs it.  All game functionality is in game.py.  main.py is just an entry point.  Looks cleaner.

--- main.py
+++ main.py
@@ -1,37 +1,8 @@
-from game import Blackjack
+from game import Game
+
+def main():
+    game = Game()
+    game.run()
 
 if __name__ == '__main__':
-    print("********* BLACKJACK ***********")
-    print("           GAME #1             ")
-    game = Blackjack()
-    game.initGame()
-    game.drawPlayerHand()
-    game.drawDealerHand()
-
-    while game.getRunning():
-
-        while game.getPlayerTurn():
-            choice = input("What would you like to do?\n1. Hit\n2. Stand\n3. Stats\n4. Quit\n")
-            if choice == "1":
-                game.hitPlayer()
-                game.check_winner()
-            elif choice == "2":
-                game.setPlayerTurn(False)
-            elif choice == "3":
-                game.showStats()
-            elif choice == "4":
-                game.setRunning(False)
-                print("\nThanks for playing!\n")
-                game.showStats()
-                break
-            else:
-                print("Invalid input! Enter 1, 2, 3, or 4")
-
-            if game.getRunning() and not game.getPlayerTurn():
-                game.setDealerTurn(True)
-                print(f"Dealer has {game.getDealerHand()[0][0]}{game.getDealerHand()[0][1]} and {game.getDealerHand()[1][0]}{game.getDealerHand()[1][1]}")
-                print(f"Dealer Hand Total: {game.getDealerHandVal()}\n")
-                game.check_dealer_blackjack()
-            while game.getDealerTurn():
-                game.hitDealer()
-                game.check_winner()
+    main()
